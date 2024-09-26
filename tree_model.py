bcfe037404ed298f4ed2b83acb823730125e7a79
Alohamaan <roginifa5678@gmail.com>
2024-09-26 20:31:46 +0300

    got 0.93 accuracy with over 0.7 precision and recall

---
+++ tree_model.py
@@ -0,0 +1,26 @@
+from main import df
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from warnings import simplefilter
+from sklearn.ensemble import RandomForestClassifier
+
+simplefilter('ignore')
+
+class TreeTest:
+
+    def test_supplierquality(df):
+        tree = RandomForestClassifier(random_state=52, n_estimators=100)
+        x = df[['DefectRate', 'SupplierQuality', 'EnergyConsumption', 'MaintenanceHours', 'QualityScore']]
+        y = df['DefectStatus']
+
+        X_train, X_holdout, y_train, y_holdout = train_test_split(x, y,
+                                                                  test_size=0.3,
+                                                                  random_state=17)
+
+        tree.fit(X_train, y_train)
+
+        return classification_report(y_holdout, tree.predict(X_holdout))
+
+print(TreeTest.test_supplierquality(df=df))
+
