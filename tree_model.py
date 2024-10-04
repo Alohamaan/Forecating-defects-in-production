d9a47cf3390dbc084409d8d86008d01e17415180
Alohamaan <roginifa5678@gmail.com>
2024-10-04 20:32:41 +0300

    got ~1 for every parameter (0.998+) by creating VotingClassifier and dealing with class imbalance, also a little rewrote main.py

--- tree_model.py
+++ tree_model.py
@@ -1,26 +1,67 @@
 from main import df
-from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
-import pandas as pd
-from warnings import simplefilter
-from sklearn.ensemble import RandomForestClassifier
+from abc import ABC, abstractmethod
 
+from sklearn.metrics import classification_report, f1_score
+from sklearn.model_selection import train_test_split
+
+from sklearn.ensemble import (RandomForestClassifier, HistGradientBoostingClassifier,
+                              VotingClassifier, ExtraTreesClassifier, BaggingClassifier)
+
+from imblearn.under_sampling import NearMiss
+from imblearn.pipeline import Pipeline
+from imblearn.over_sampling import RandomOverSampler
+
+from warnings import simplefilter
 simplefilter('ignore')
 
-class TreeTest:
+class Model(ABC):
 
-    def rand_forest(df):
-        tree = RandomForestClassifier(random_state=52, n_estimators=100)
-        x = df[['DefectRate', 'SupplierQuality', 'EnergyConsumption', 'MaintenanceHours', 'QualityScore']]
-        y = df['DefectStatus']
+    @abstractmethod
+    def CreateTest(df, name='DefectStatus'):
 
-        X_train, X_holdout, y_train, y_holdout = train_test_split(x, y,
-                                                                  test_size=0.3,
-                                                                  random_state=17)
+        """Creates test, using every column in dataframe except one, if not mentioned, except DefectStatus,
+        also reduces class imbalance"""
 
-        tree.fit(X_train, y_train)
+        x = df[[x for x in df.columns if x != name]]
+        y = df[name]
 
-        return classification_report(y_holdout, tree.predict(X_holdout))
+        r_state = 52
+        pipe = Pipeline([('over', RandomOverSampler(random_state=r_state)), ('nearmiss', NearMiss())])
+        x, y = pipe.fit_resample(x, y)
 
-print(TreeTest.rand_forest(df=df))
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=17)
 
+        return [X_train, X_test, y_train, y_test, r_state]
+
+    @abstractmethod
+    def learn(df):
+        pass
+
+    @abstractmethod
+    def CreateEstimator():
+
+        """Creates list of estimators, consists of next ML algorithms: RandomForestClassifier, ExtraTreesClassifier,
+        HistGradientBoostingClassifier, BaggingClassifier"""
+
+        estimator = []
+
+        estimator.append(('RFC', RandomForestClassifier(random_state=52)))
+        estimator.append(('EXT', ExtraTreesClassifier(random_state=52)))
+        estimator.append(('HGBC', HistGradientBoostingClassifier(random_state=52)))
+        estimator.append(('BGC', BaggingClassifier(random_state=52)))
+
+        return estimator
+
+class Voting(Model):
+
+    def learn(df, type='soft'):
+
+        X_train, X_test, y_train, y_test, r_state = Model.CreateTest(df=df)
+
+        vot = VotingClassifier(estimators=Model.CreateEstimator(), voting=type,
+                               n_jobs=-1, weights=[4,3,2,1])
+
+        vot.fit(X_train, y_train)
+        return f1_score(y_test, vot.predict(X_test))
+
+print(Voting.learn(df=df))
